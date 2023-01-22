bfc9f0833312dda1e52db218d956e3918b209e20
Blessing Edidiong Johnson <97248332+BlueJohnson@users.noreply.github.com>
2023-01-22 18:25:31 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        colors: {
+            black: "#161722",
+            primary: "#FFD2A4",
+            light_orange: "#FFF5EB",
+            gray: "#F5F2F0",
+            white: "#FFFFFF",
+            primary_hover: "#F2C79B",
+            primary_dark: "#A54E2B",
+            light_b: "rgb(226, 227, 228)",
+        },
+    },
+};
